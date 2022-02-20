c050353760a6dfd11a6a10fdd8a03d06f400c3a9
Andy Pearson <andy@londonparkour.com>
2022-02-20 10:29:12 +0000

    remove views

--- src/views/single-parts/content/content.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$content = apply_filters('cpt_syllabus_transforms', $post->post_content);
-
-// echo '<div class="text-2xl mb-4">Description</div>';
-
-// echo '<hr class="mb-4"/>';
-
-echo $content;
--- src/views/single-parts/content/subtitle.php
+++
@@ -1,5 +0,0 @@
-<h2 class="text-4xl text-fog mb-10 capitalize ">
-    <?php the_title(); ?>
-</h2>
-
-<hr class="mb-10"/>
