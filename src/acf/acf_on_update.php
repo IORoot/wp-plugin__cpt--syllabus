d4292d09710c16a882bb88084105e872cc1746a7
Andy Pearson <andy@londonparkour.com>
2022-04-13 13:04:05 +0100

    reedo

--- src/acf/acf_on_update.php
+++ src/acf/acf_on_update.php
@@ -1,18 +1,45 @@
 <?php
 
 /**
- * On save of options page, create the category.
+ * Update the youtube data with a response from the 
+ * youtube API. (using the Youtube_api class)
+ *
+ * @param [type] $value
+ * @param [type] $post_id
+ * @param [type] $field
+ * @param [type] $original
+ * @return void
  */
-function save_regexpost_options() {
+function my_acf_update_value( $value, $post_id, $field, $original ) {
 
-    $screen = get_current_screen();
-	if (strpos($screen->id, "regexposts") == false) { return; }
+    if ($field['name'] != 'media'){ return $value; }
 
-    // Create new object.
-    $rp = new \andyp\regexer\lib\regexer();
-    $rp->run();
+    // get sub-field IDs - this is because the field_key will
+    // change - so don't rely on it!
+    $youtube_id_key   = $field["sub_fields"][0]['key'];
+    $youtube_json_key = $field["sub_fields"][1]['key'];
+    $post_data = $_POST["acf"][$field["key"]];
 
-	return;
+    $index=1;
+    foreach ($post_data as $post_index => $post_row)
+    {
+        if (empty($post_row[$youtube_id_key])){ $index++; continue; }
+
+        if (empty($post_row[$youtube_json_key])){
+            // If the field is empty, get the data.
+            $youtube_api = new andyp\theme\syllabus\lib\youtube_api;
+            $data = $youtube_api->get_data($post_row[$youtube_id_key]);
+            $json = json_encode($data);
+
+            $result = update_sub_field( array('media', $index, 'video_details'), $json );
+        }
+
+        $index++;
+    }
+
+    return $value;
+
 }
 
-add_action('acf/save_post', 'save_regexpost_options', 20);
+// Apply to all fields.
+add_filter('acf/update_value', 'my_acf_update_value', 10, 4);
